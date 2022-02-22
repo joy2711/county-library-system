9a6c445a6635b22529760072645388ea0bba64a4
Joy Anne Wairimu <60652642+joy2711@users.noreply.github.com>
2022-02-22 23:49:21 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+Project : County Library System
+Author : Joy Anne Wairimu Kamau
+Date : 1/2/22
+Compiler : GNU GCC
+Language : C89
+License : MIT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("County Library\n");
+    printf("Welcome to our county library\n");
+    printf("How may we be of help?\n");
+    printf("1.Add new user\n");
+    printf("2.Add new book\n");
+    printf("Selected Action");
+
+}
